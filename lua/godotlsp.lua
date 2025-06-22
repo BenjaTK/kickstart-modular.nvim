67f35215c449040735c00943875aa504207c93d4
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2025-06-22 15:44:50 -0300

    Actually add Godot LSP

---
+++ lua/godotlsp.lua
@@ -0,0 +1,20 @@
+local cmd = vim.lsp.rpc.connect('127.0.0.1', 6005)
+local pipe = '/tmp/godot.pipe'
+local function start_godot_lsp()
+  vim.lsp.start {
+    name = 'Godot',
+    cmd = cmd,
+    root_dir = vim.fs.dirname(vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]),
+    on_attach = function(client, bufnr)
+      vim.api.nvim_command('echo serverstart("' .. pipe .. '")')
+    end,
+  }
+end
+
+-- Set up an autocommand for GDScript file type
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'gdscript',
+  callback = function()
+    start_godot_lsp()
+  end,
+})
