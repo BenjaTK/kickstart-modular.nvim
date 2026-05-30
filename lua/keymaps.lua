a4e179e0fee9790f7ba25072a07f2611505dc62c
BenjaTK <73806216+BenjaTK@users.noreply.github.com>
2026-05-30 12:24:18 -0300

    Add eyeliner and marks

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -36,7 +36,7 @@ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
+vim.keymap.set('n', '?', ':WhichKey<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
--- lua/options.lua
+++ lua/options.lua
@@ -17,6 +17,7 @@ vim.o.showmode = false
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.wrap = false
+vim.o.swapfile = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
@@ -71,5 +72,9 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+vim.diagnostic.config {
+  signs = false,
+}
 
 -- vim: ts=2 sts=2 sw=2 et
